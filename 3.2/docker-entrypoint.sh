4a95cf53f537fbbaad8cff591684801efc261daf
zhouyq <zyq916@gmail.com>
2016-05-12 17:25:05 +0800

    remove BASE_SOURCE

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -20,7 +20,7 @@ done
 # allow the container to be started with `--user`
 if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
 	chown -R mongodb.mongodb $DATADIR
-	exec gosu mongodb "$BASH_SOURCE" "$@"
+	exec gosu mongodb "$@"
 fi
 
 if [ "$1" = 'mongod' ]; then
