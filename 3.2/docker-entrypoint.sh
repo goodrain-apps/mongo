d53fc036c131098c25b57279547f428269b025f1
zjhgoodrain <38780264+zjhgoodrain@users.noreply.github.com>
2018-06-25 12:41:05 +0800

    Update docker-entrypoint.sh

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -5,7 +5,8 @@
 # read env set mongo config file
 source /tmp/bin/set_config_file.sh
 
-sed -i "s/DB_PATH/${DB_PATH}/g" /etc/mongod.conf
+set -xe
+sed -i "s/DB_PATH/${DB_PATH}/g" etc/mongod.conf
 
 set -e
 
